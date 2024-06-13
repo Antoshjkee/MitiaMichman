a9b1894efbd9f2597cb6ff9e0d92ea7d7c795083
Anton Pankov <antoshjke@gmail.com>
2024-06-13 20:52:10 +0100

    Add project files.

---
+++ MitiaMichman/Person.cs
@@ -0,0 +1,38 @@
+﻿using System.Dynamic;
+
+namespace MitiaMichman;
+
+internal class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public bool HasCats { get; set; }
+
+    public Person(string name, int age)
+    {
+        if (age < 18)
+        {
+            throw new Exception("Person is under age");
+        }
+
+        Name = name;
+        Age = age;
+    }
+
+    public Person(string name, int age, bool hasCats) 
+    {
+        if (age < 18)
+        {
+            throw new Exception("Person is under age");
+        }
+
+        Name = name;
+        Age = age;
+        HasCats = hasCats;
+    }
+
+    public void SayMyName()
+    {
+        Console.WriteLine($"MY NAME IS {Name}");
+    }
+}
---
+++ MitiaMichman/Program.cs
@@ -0,0 +1,59 @@
+﻿using MitiaMichman;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hi, whats your name?");
+        string nameFromConsole = Console.ReadLine();
+
+        Console.WriteLine($"{nameFromConsole}, how old are you?");
+        string ageFromConsole = Console.ReadLine();
+
+        int age = int.Parse(ageFromConsole);
+        Person mitia = new(nameFromConsole, age, false);
+
+        Console.WriteLine($"Hi {mitia.Name}");
+        Console.ReadLine();
+
+        mitia.SayMyName();
+        mitia.SayMyName();
+        mitia.SayMyName();
+
+    }
+
+    //    Person mitia = new();
+
+    //Console.WriteLine("Hi, whats your name?");
+    //mitia.Name = Console.ReadLine();
+
+    //Console.WriteLine($"{mitia.Name}, how old are you?");
+    //string stringNumber = Console.ReadLine();
+
+    //bool isValidNumber = int.TryParse(stringNumber, out int age);
+
+    //if (isValidNumber)
+    //{
+    //    mitia.Age = age;
+    //    Console.WriteLine($"WOW {mitia.Name}, you are {mitia.Age}???");
+
+    //    if (age < 18)
+    //    {
+    //        Console.WriteLine("You are too young");
+    //    }
+    //    else if (age >= 18 && age <= 65)
+    //    {
+    //        Console.WriteLine("Go back to work");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Chill brother");
+    //    }
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Age is incorrect");
+    //}
+
+}
